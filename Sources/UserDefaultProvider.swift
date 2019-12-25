c2c8e617ad930a944687b025eb24f18b7c4b6ae6
Serhii Horielov <s.horielov@alterplay.com>
2019-12-25 15:02:45 +0200

    move pod internals on a podspec level

--- Sources/UserDefaultProvider.swift
+++ Sources/UserDefaultProvider.swift
@@ -7,8 +7,6 @@
 //
 
 import Foundation
-    
-typealias StorageKey = String
 
 protocol UserDefaultsProvider {
 	func bool(forKey defaultName: StorageKey) -> Bool
@@ -16,29 +14,29 @@ protocol UserDefaultsProvider {
     func URL(forKey defaultName: StorageKey) -> URL?
     
     func setURL(_ url: URL?, forKey defaultName: StorageKey)
-    func setValue(_ value: Any?, forKey defaultName: StorageKey)
+    func set(_ value: Any?, forKey defaultName: StorageKey)
     func value(forKey key: String) -> Any?
 }
 
 extension UserDefaults: UserDefaultsProvider {
     
-    func setValue(_ value: Any?, forKey defaultName: StorageKey) {
-        self.set(value, forKey: defaultName.rawValue)
+    func set(_ value: Any?, forKey defaultName: StorageKey) {
+        self.set(value, forKey: defaultName)
     }
     
     func bool(forKey defaultName: StorageKey) -> Bool {
-        return self.bool(forKey: defaultName.rawValue)
+        return self.bool(forKey: defaultName)
     }
     
     func string(forKey defaultName: StorageKey) -> String? {
-        return self.string(forKey: defaultName.rawValue)
+        return self.string(forKey: defaultName)
     }
     
     func URL(forKey defaultName: StorageKey) -> URL? {
-        return self.url(forKey: defaultName.rawValue)
+        return self.url(forKey: defaultName)
     }
     
     func setURL(_ url: URL?, forKey defaultName: StorageKey) {
-        self.set(url, forKey: defaultName.rawValue)
+        self.set(url, forKey: defaultName)
     }
 }
